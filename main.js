e279987cba7e2ff866a1c010811ae3cbd749367b
Nicholas Glover <jacksaintjack@gmail.com>
2015-06-05 14:20:51 -0400

    got the Fish page to work.Still need to add some design to it.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-  $('.row').on('click', 'a', function (event) {
+  $('#landingPage').on('click', 'a', function (event) {
     event.preventDefault();
     console.log("this element ",$(this).attr('rel'));
     var clickedPage = $(this).attr('rel');
     $(clickedPage).siblings().removeClass('active');
     $(clickedPage).addClass('active');
   });
+
+
 
 });
