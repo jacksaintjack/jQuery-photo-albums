e706c48ba41a5f3b22959185a6bc9d2c69a7b222
Nicholas Glover <jacksaintjack@gmail.com>
2015-06-04 20:58:11 -0400

    got part of the activate funciton to work. Will need to do some css

---
+++ data.js
@@ -0,0 +1,5 @@
+var templates = {};
+
+templates.fishLink = [
+  "<h1>More About Me</h1>",
+].join("");
--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-$(document).ready(function() {}
+$(document).ready(function() {
+
+  $('.row').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+});
