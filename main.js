5ef289aaf5deabebc7458236ca44d8a0554e4801
Nicholas Glover <jacksaintjack@gmail.com>
2015-06-06 20:35:36 -0400

    All albums complete including the buttons for the sides.

--- main.js
+++ main.js
@@ -7,7 +7,55 @@ $(document).ready(function() {
     $(clickedPage).siblings().removeClass('active');
     $(clickedPage).addClass('active');
   });
-  
+
+  $('#fishPage').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#japanPage').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#ziltoidPage').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#birdPage').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#oldPage').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#wierdPage').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
 
 
 
