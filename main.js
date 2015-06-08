d4b0c74a90ae54d4b962de61f00cd2271b26c1e4
Nicholas Glover <jacksaintjack@gmail.com>
2015-06-07 21:00:25 -0400

    Removed all alignment background-colors, and finished my side bar navigation for in larged picture buttons.

--- main.js
+++ main.js
@@ -149,6 +149,194 @@ $(document).ready(function() {
     $(clickedPage).addClass('active');
   });
 
+  //Ziltoid Selection//
+  $('#pictureOneZiltoid').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureTwoZiltoid').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureThreeZiltoid').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureFourZiltoid').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureFiveZiltoid').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureSixZiltoid').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  //Bird Selection//
+  $('#pictureOneBird').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureTwoBird').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureThreeBird').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureFourBird').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureFiveBird').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureSixBird').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  //Section for in largment of Old Pictures//
+
+  $('#pictureOneOld').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureTwoOld').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureThreeOld').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureFourOld').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureFiveOld').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureSixOld').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+//Section for Wierd Inlargement//
+  $('#pictureOneWierd').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureTwoWierd').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureThreeWierd').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+
+  $('#pictureFourWierd').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureFiveWierd').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
+  $('#pictureSixWierd').on('click', 'a', function (event) {
+    event.preventDefault();
+    console.log("this element ",$(this).attr('rel'));
+    var clickedPage = $(this).attr('rel');
+    $(clickedPage).siblings().removeClass('active');
+    $(clickedPage).addClass('active');
+  });
 
 
 
